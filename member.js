1c1a0f27a6224f02d788a4345b5ca596d7e0cace
Shweta Singh <103261868+shwetasng@users.noreply.github.com>
2023-07-21 18:23:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: ['JavaScript', 'CSS', 'HTML'],
+        calculateNumbers: function(var1, var2) {
+            return var1 + var2;
+        }
+    }
+    return member;
+}
